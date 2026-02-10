c448d47f4dbd71494cf6f1fdd0b06f61f20d6ade
egyrmexiq-oss <egyrmexiq@gmail.com>
2026-02-10 14:06:02 -0600

    Update GenerativeModel to use gemini-2.5-flash

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ ACCESO_KEYS = st.secrets.get("access_keys", {})
 
 #if GOOGLE_API_KEY:
     #genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 #else:
     #st.error("Falta API Key")
     #st.stop()
